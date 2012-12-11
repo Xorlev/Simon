52df005006cc7048ce2ed805dc2452ae416de7c0
Michael Rose <elementation@gmail.com>
2012-12-11 00:56:24 -0700

    Pulling render out into function for further abstraction into render pipeline for JSON/XML processing

--- src/main/scala/com/xorlev/simon/handlers/DynamicMethodHandler.scala
+++ src/main/scala/com/xorlev/simon/handlers/DynamicMethodHandler.scala
@@ -33,12 +33,12 @@ class DynamicMethodHandler extends RequestHandler {
     }
   }
 
-  def get(path: String)(f: =>Any) = ctx.put(("GET", path), x=>f.toString)
-  def post(path: String)(f: =>Any) = ctx.put(("POST", path), x=>f.toString)
-  def put(path: String)(f: =>Any) = ctx.put(("PUT", path), x=>f.toString)
-  def delete(path: String)(f: =>Any) = ctx.put(("DELETE", path), x=>f.toString)
-  def options(path: String)(f: =>Any) = ctx.put(("OPTIONS", path), x=>f.toString)
-  def head(path: String)(f: =>Any) = ctx.put(("HEAD", path), x=>f.toString)
+  def get(path: String)(f: =>Any) = ctx.put(("GET", path), x=>doRender(f))
+  def post(path: String)(f: =>Any) = ctx.put(("POST", path), x=>doRender(f))
+  def put(path: String)(f: =>Any) = ctx.put(("PUT", path), x=>doRender(f))
+  def delete(path: String)(f: =>Any) = ctx.put(("DELETE", path), x=>doRender(f))
+  def options(path: String)(f: =>Any) = ctx.put(("OPTIONS", path), x=>doRender(f))
+  def head(path: String)(f: =>Any) = ctx.put(("HEAD", path), x=>doRender(f))
 
   def runRoute(request: HttpRequest, route: (String, String)): String = {
     log.debug("Running route {}", route)
@@ -46,5 +46,9 @@ class DynamicMethodHandler extends RequestHandler {
     paramsMap.withValue(mutable.Map(request.params.toSeq: _*)) {
       ctx(route)()
     }
+  }
+  
+  def doRender(f: =>Any): String = {
+    f.toString
   }
 }
