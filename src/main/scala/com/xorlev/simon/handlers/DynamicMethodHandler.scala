1b7044a9c547770bf663393d1d70536336c08135
Michael Rose <elementation@gmail.com>
2012-12-13 00:54:05 -0700

    Upgrading handler to wire in the request

--- src/main/scala/com/xorlev/simon/handlers/DynamicMethodHandler.scala
+++ src/main/scala/com/xorlev/simon/handlers/DynamicMethodHandler.scala
@@ -18,36 +18,44 @@ import java.io.ByteArrayInputStream
 
 class DynamicMethodHandler extends RequestHandler {
   val ctx = new HashMap[(String, String), (Any=>HttpResponse)]
-
-  val paramsMap = new DynamicVariable[mutable.Map[String,String]](null)
-
   val mapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
 
-  implicit def params: mutable.Map[String, String] = paramsMap.value
+  val paramsMap = new DynamicVariable[mutable.Map[String,String]](null)
+  val requestVar = new DynamicVariable[HttpRequest](null)
 
+  implicit def params: mutable.Map[String, String] = paramsMap.value
+  implicit def request: HttpRequest = requestVar.value
+
+
+  /**
+   * Handles request and invokes a handler, or else it returns a 404 response
+   * @param request
+   * @return HttpResponse
+   */
   override def handleRequest(request: HttpRequest): Option[HttpResponse] = {
     val r = request.request
 
     if (ctx.isDefinedAt((r.method, r.resource))) {
       Some(runRoute(request, (r.method, r.resource)))
     } else {
-      Some(HttpResponse(404, MimeUtil.HTML, RenderUtil.notFound()))
+      Some(NotFound(RenderUtil.notFound()))
     }
   }
 
-  def get(path: String)(f: =>Any) = ctx.put(("GET", path), x=>doRender(f))
-  def post(path: String)(f: =>Any) = ctx.put(("POST", path), x=>doRender(f))
-  def put(path: String)(f: =>Any) = ctx.put(("PUT", path), x=>doRender(f))
-  def delete(path: String)(f: =>Any) = ctx.put(("DELETE", path), x=>doRender(f))
-  def options(path: String)(f: =>Any) = ctx.put(("OPTIONS", path), x=>doRender(f))
-  def head(path: String)(f: =>Any) = ctx.put(("HEAD", path), x=>doRender(f))
-
+  /**
+   * Responsible for matching the route and executing it with the proper DynamicVariables instantiated
+   * @param request
+   * @param route
+   * @return
+   */
   def runRoute(request: HttpRequest, route: (String, String)): HttpResponse = {
     log.debug("Running route {}", route)
     log.debug("Desired Content-type: " + request.getContentType)
     paramsMap.withValue(parseParams(request.params)) {
-      ctx(route)()
+      requestVar.withValue(request) {
+        ctx(route)()
+      }
     }
   }
 
@@ -68,4 +76,11 @@ class DynamicMethodHandler extends RequestHandler {
       case _ => HttpResponse(400, MimeUtil.HTML, RenderUtil.badRequest())
     }
   }
+
+  def get(path: String)(f: =>Any) = ctx.put(("GET", path), x=>doRender(f))
+  def post(path: String)(f: =>Any) = ctx.put(("POST", path), x=>doRender(f))
+  def put(path: String)(f: =>Any) = ctx.put(("PUT", path), x=>doRender(f))
+  def delete(path: String)(f: =>Any) = ctx.put(("DELETE", path), x=>doRender(f))
+  def options(path: String)(f: =>Any) = ctx.put(("OPTIONS", path), x=>doRender(f))
+  def head(path: String)(f: =>Any) = ctx.put(("HEAD", path), x=>doRender(f))
 }
